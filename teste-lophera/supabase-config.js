73f59bdabc16c63a33401717b4b2de25f0767ec4
LopheraStore <lopherastore@gmail.com>
2026-09-17 14:27:46 -0300

    Teste Lophera: configuração limpa do Supabase

---
+++ teste-lophera/supabase-config.js
@@ -0,0 +1,3 @@
+const LOPHERA_SUPABASE_URL='https://zngimouhumgjcvwsnvkv.supabase.co';
+const LOPHERA_SUPABASE_KEY='sb_publishable_MF5TQN28QwMUFewXEUp9qw_l4MkNU0T';
+const supabaseClient=window.supabase.createClient(LOPHERA_SUPABASE_URL,LOPHERA_SUPABASE_KEY);
